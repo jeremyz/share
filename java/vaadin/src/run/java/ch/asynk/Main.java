6437ef7aff1291f74ddbcf0ab04aa0a7e346a591
Jérémy Zurcher <jeremy@asynk.ch>
2017-03-22 09:59:38 +0100

    java : vaadin : add description to jar target, add msg stating that HelloWorldServlet is not used

--- java/vaadin/src/run/java/ch/asynk/Main.java
+++ java/vaadin/src/run/java/ch/asynk/Main.java
@@ -6,6 +6,7 @@ public class Main
 {
     public static void main(String[] args) throws Exception
     {
+        System.err.println("!!! org.vaadin.jetty.VaadinJettyServer uses it's own VaadinServlet instance instead of HelloWorldServlet.\n");
         String webRoot = System.getProperty("WEBROOT");
         if (webRoot == null) webRoot = "./src/main/WebContent/";
         new VaadinJettyServer(8080, HelloWorld.class, webRoot).start();
