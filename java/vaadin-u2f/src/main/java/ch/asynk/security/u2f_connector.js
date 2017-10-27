7c0b9a4841b0713f63857584ecb593883bb8ad61
Jérémy Zurcher <jeremy@asynk.ch>
2017-10-27 15:30:55 +0200

    u2f_connector.js : fix u2f js calls, supports chrome

--- java/vaadin-u2f/src/main/java/ch/asynk/security/u2f_connector.js
+++ java/vaadin-u2f/src/main/java/ch/asynk/security/u2f_connector.js
@@ -6,25 +6,25 @@ window.ch_asynk_security_U2fConnector = function() {
         var request = JSON.parse(requestJson);
         // alert(JSON.stringify(request));
         setTimeout(function() {
-            u2f.register(request.registerRequests, request.authenticateRequests, function(data) {
+            u2f.register(request.registerRequests[0].appId, request.registerRequests, request.authenticateRequests, function(data) {
                 if(data.errorCode)
                     connector.onRegisterResponse(data.errorCode, request);
                 else
                     connector.onRegisterResponse(JSON.stringify(data));
             });
-        }, 500);
+        }, 5000);
     }
 
     this.authenticate = function(requestJson) {
         var request = JSON.parse(requestJson);
-        //alert(JSON.stringify(request));
+        // alert(JSON.stringify(request));
         setTimeout(function() {
-            u2f.sign(request.authenticateRequests, function(data) {
+            u2f.sign(request.authenticateRequests[0].appId, request.authenticateRequests[0].challenge, request.authenticateRequests, function(data) {
                 if(data.errorCode)
                     connector.onAuthenticateResponse(data.errorCode, request);
                 else
                     connector.onAuthenticateResponse(JSON.stringify(data));
             });
-        }, 500);
+        }, 5000);
     }
 }
